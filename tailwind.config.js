40d3299ae5e1d603ff44ddf0ab8f4e22c6e89e38
resumebot-ai <resumebot.ai.team@gmail.com>
2025-07-20 22:33:27 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './styles/**/*.{js,ts,jsx,tsx,css}',
+  ],
   theme: {
     extend: {},
   },
